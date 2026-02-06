94e46fb0db1020dcb37ffd76597e117ee06dbc94
moallemm <mohamadmoallem79@gmail.com>
2026-02-06 15:58:43 +0200

    Complete project: scripts, documentation, license

---
+++ scripts/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Run in Terminal 1: ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py"
+echo "Run in Terminal 2: ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True"
+echo ""
+echo "IN RViz: 1. Click 2D Pose Estimate, 2. Click Nav2 Goal"
